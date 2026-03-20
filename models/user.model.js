7a2ff0a894dbf66e8370c62eb8af9b17a5325fc7
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-03-20 17:59:38 +0530

    feature: Add User model

---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'First name is required'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Last name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: (value) => validator.isEmail(value),
+        message: 'Please provide a valid email address',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minLength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
